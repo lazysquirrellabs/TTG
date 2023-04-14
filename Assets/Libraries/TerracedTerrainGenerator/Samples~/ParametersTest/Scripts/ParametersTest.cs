6f6509bc35c7e8b6162add8f3dd60cb3dc9ae417
Matheus Amazonas <matheus.amazonas@gmail.com>
2023-04-14 20:28:41 +0200

    🔧 Fix namespaces in the samples folder

--- Assets/Libraries/TerracedTerrainGenerator/Samples~/ParametersTest/Scripts/ParametersTest.cs
+++ Assets/Libraries/TerracedTerrainGenerator/Samples~/ParametersTest/Scripts/ParametersTest.cs
@@ -1,7 +1,7 @@
 using System.Threading;
 using UnityEngine;
 
-namespace SneakySquirrelLabs.TerracedTerrainGenerator.Samples
+namespace SneakySquirrelLabs.TerracedTerrainGenerator.Samples.ParametersTest
 {
 	public class ParametersTest : MonoBehaviour
 	{
