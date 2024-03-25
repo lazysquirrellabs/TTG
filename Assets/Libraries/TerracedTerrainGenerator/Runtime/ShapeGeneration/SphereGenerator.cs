2af07e5a9f877f6f9d805f7eea1b40dcc4bcdc09
Matheus Amazonas <matheus.amazonas@gmail.com>
2024-03-25 19:29:46 +0100

    ♻️ Make Sphere Generator a Generator
    So it's not a helper class anymore.

--- Assets/Libraries/TerracedTerrainGenerator/Runtime/ShapeGeneration/SphereGenerator.cs
+++ Assets/Libraries/TerracedTerrainGenerator/Runtime/ShapeGeneration/SphereGenerator.cs
@@ -1,17 +1,37 @@
+using LazySquirrelLabs.TerracedTerrainGenerator.Data;
+using Unity.Collections;
 using UnityEngine;
 
-namespace LazySquirrelLabs.TerracedTerrainGenerator.SphereGeneration
+namespace LazySquirrelLabs.TerracedTerrainGenerator.ShapeGeneration
 {
-	internal class SphereGenerator : MonoBehaviour
+	internal class SphereGenerator : ShapeGenerator
 	{
-		[SerializeField] private MeshFilter _otherMeshFilter;
-
-		private readonly int[] _indices = {
-			0, 1, 2, 0, 3, 1, 0, 2, 4, 3, 0, 5, 0, 4, 5, 1, 3, 6, 1, 7, 2, 7, 1, 6, 4, 2, 8, 7, 8, 2, 9, 3, 5, 6, 3, 9,
-			5, 4, 10, 4, 8, 10, 9, 5, 10, 7, 6, 11, 7, 11, 8, 11, 6, 9, 8, 11, 10, 10, 11, 9
+		#region Fields
+		
+		private static readonly int[] Indices = {
+			0, 1, 2, 
+			0, 3, 1, 
+			0, 2, 4, 
+			3, 0, 5, 
+			0, 4, 5, 
+			1, 3, 6, 
+			1, 7, 2, 
+			7, 1, 6, 
+			4, 2, 8, 
+			7, 8, 2, 
+			9, 3, 5, 
+			6, 3, 9,
+			5, 4, 10, 
+			4, 8, 10, 
+			9, 5, 10, 
+			7, 6, 11, 
+			7, 11, 8, 
+			11, 6, 9, 
+			8, 11, 10, 
+			10, 11, 9
 		};
 		
-		private readonly Vector3[] _vertices =
+		private static readonly Vector3[] Vertices =
 		{
 			new(0.8506508f, 0.5257311f, 0f),
 			new(0.000000101405476f, 0.8506507f, -0.525731f),
@@ -24,17 +44,38 @@ namespace LazySquirrelLabs.TerracedTerrainGenerator.SphereGeneration
 			new(-0.5257309f, 0.00000006267203f, 0.85065067f),
 			new(-0.000000101405476f, -0.8506506f, -0.525731f),
 			new(-0.000000101405476f, -0.8506507f, 0.525731f),
-			new(-0.8506508f, -0.5257311f, 0f),
+			new(-0.8506508f, -0.5257311f, 0f)
 		};
-		
-		private void Start()
+
+		private readonly float _radius;
+
+		#endregion
+
+		#region Setup
+
+		public SphereGenerator(float radius)
 		{
-			var mesh = new Mesh();
-			mesh.SetVertices(_vertices);
-			mesh.SetIndices(_indices, MeshTopology.Triangles, 0);
-			mesh.RecalculateNormals();
-			mesh.RecalculateBounds();
-			_otherMeshFilter.mesh = mesh;
+			_radius = radius;
 		}
+
+		#endregion
+
+		#region Internal
+
+		internal override SimpleMeshData Generate(Allocator allocator)
+		{
+			var vertices = new NativeArray<Vector3>(Vertices, allocator);
+			for (var i = 0; i < Vertices.Length; i++)
+				vertices[i] = Vertices[i] * _radius;
+			var indices = new NativeList<int>(Indices.Length, allocator);
+			foreach (var t in Indices)
+				indices.Add(t);
+
+			return new SimpleMeshData(vertices, indices);
+		}
+
+		#endregion
+
+		
 	}
 }
