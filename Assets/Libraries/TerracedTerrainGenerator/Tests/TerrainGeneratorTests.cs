c01f6252ea5f43b5e058c7c5524e16a35df80281
Matheus Amazonas <matheus.amazonas@gmail.com>
2024-08-05 16:59:35 +0200

    ✅ Add automated tests for Spherical Terrain Generator's constructor
    Just like there were tests for the planar one.

--- Assets/Libraries/TerracedTerrainGenerator/Tests/TerrainGeneratorTests.cs
+++ Assets/Libraries/TerracedTerrainGenerator/Tests/TerrainGeneratorTests.cs
@@ -61,5 +61,53 @@ namespace LazySquirrelLabs.TerracedTerrainGenerator.Tests
 				_ = new PlanarTerrainGenerator(sides, radius, maximumHeight, heights, settings, depth);
 			}
 		}
+		
+		[Test]
+		public void SphericalConstructor()
+		{
+			var maximumHeight = 10f;
+			var heights = new[] { 0.1f, 0.2f, 0.9f };
+			var settings = new SculptSettings(0.1f, 2, 0.5f, 1.5f, AnimationCurve.Linear(0, 0, 1, 1));
+			ushort depth = 3;
+			
+			// Invalid minimum height
+			var minimumHeight = -40f;
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			minimumHeight = 0;
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			minimumHeight = 10;
+			// Invalid maximum height
+			maximumHeight = -10f;
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			maximumHeight = 0f;
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			// Grater minimum height
+			minimumHeight = 20;
+			maximumHeight = 10;
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			maximumHeight = 25f;
+			// Invalid custom heights
+			heights = Array.Empty<float>();
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			heights = new[] { 0.5f, 0.3f };
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			heights = new[] { -0.1f };
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			heights = new[] { 1.1f };
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			heights = new[] { 0.1f, 0.2f, 0.3f };
+			// Invalid depth
+			depth = 0;
+			Assert.Throws<ArgumentOutOfRangeException>(Create);
+			depth = 3;
+			// Nothing wrong, just to ensure no other parameters were still invalid
+			Create();
+			return;
+
+			void Create()
+			{
+				_ = new SphericalTerrainGenerator(minimumHeight, maximumHeight, heights, settings, depth);
+			}
+		}
 	}
 }
